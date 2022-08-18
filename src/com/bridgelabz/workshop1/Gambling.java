b235e8c11abca4add29feb680830534c1ed95425
mukeshkapse <mukeshkapse1832@gmail.com>
2022-08-18 18:42:14 +0530

    Added: Win And Loss

--- src/com/bridgelabz/workshop1/Gambling.java
+++ src/com/bridgelabz/workshop1/Gambling.java
@@ -1,10 +1,21 @@
 package com.bridgelabz.workshop1;
 
 public class Gambling {
-   static final int Stack = 100;
-   static final int bet = 1;
+   static int stack = 100;
+   static int bet = 1;
+   static int win = 1;
     public static void main(String[] args) {
         System.out.println("Welcome to the Gabling");
+
+        int bit = (int) (Math.random() * (2) + 1);
+        if (bit == win) {
+            System.out.println("You Are Win");
+            stack++;
+        } else {
+            System.out.println("You Are Loss");
+            stack--;
+        }
+        System.out.println("Stack is "+stack);
 
     }
 }
