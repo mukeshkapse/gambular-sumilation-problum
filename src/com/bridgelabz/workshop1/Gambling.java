1424553a703b70e96448f72def34d7ee92da87aa
mukeshkapse <mukeshkapse1832@gmail.com>
2022-08-18 18:15:01 +0530

    Added: Start of 100 stack every day and 1 bet for every game

--- src/com/bridgelabz/workshop1/Gambling.java
+++ src/com/bridgelabz/workshop1/Gambling.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.workshop1;
 
 public class Gambling {
+   static final int Stack = 100;
+   static final int bet = 1;
     public static void main(String[] args) {
         System.out.println("Welcome to the Gabling");
+
     }
 }
